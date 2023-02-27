45c2cb7f97eb33d103e1700d892afd15155b6da2
Huy Viet Nguyen <huy-viet.nguyen@polymtl.ca>
2023-02-26 22:07:02 -0500

    task: Tubes nommes

--- TubesNommes.c
+++ TubesNommes.c
@@ -10,13 +10,46 @@ int main() {
     pid_t P2 = 0;
     pid_t P3 = 0;
 
-    mkfifo("pipe", 0660);
+    int f = mkfifo("pipe", 0600);
+
+    if ((P2 = fork()) == 0) {            
+        if ((P1 = fork()) == 0) {
+            int fd = open("pipe", O_WRONLY);
+            dup2(fd, 1);
+            close(fd);
+            execl("/usr/bin/rev", "rev", "In.txt", NULL);
+        }
+        waitpid(P1, NULL, 0);
+
+        int fd = open("pipe", O_RDONLY);
+        int out = open("Outn.txt", O_WRONLY | O_CREAT | O_TRUNC, 0660);
+        dup2(fd, 0);
+        dup2(out, 1);
+        close(fd);
+        close(out);
+        
+        execl("/usr/bin/rev", "rev", NULL);
+        _exit(0);
+    }
+    waitpid(P2, NULL, 0);
+
+    if ((P3 = fork()) == 0) {
+        execl("/usr/bin/diff", "diff", "In.txt", "Outn.txt", "-s", NULL);
+    }
+}
+
+/*
+    pid_t P1 = 0;
+    pid_t P2 = 0;
+    pid_t P3 = 0;
+
+    int f = mkfifo("pipe", 0660);
 
     if ((P2 = fork()) == 0) {            
         if ((P1 = fork()) == 0) {        
             int fd = open("pipe", O_WRONLY);
             dup2(fd, 1);
-            // close(fd);
+            close(fd);
             execl("/usr/bin/rev", "rev", "In.txt", NULL);
         }
         waitpid(P1, NULL, 0);
@@ -35,4 +68,4 @@ int main() {
     if ((P3 = fork()) == 0) {
         execl("/usr/bin/diff", "diff", "In.txt", "Outn.txt", "-s", NULL);
     }
-}
+*/
